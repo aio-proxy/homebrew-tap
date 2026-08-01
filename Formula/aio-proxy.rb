027d9fe675077f5b2ed4baf8a57fe13c71382f73
baranwang <11427801+baranwang@users.noreply.github.com>
2026-08-01 07:16:39 +0000

    chore: update aio-proxy to v0.2.1

--- Formula/aio-proxy.rb
+++ Formula/aio-proxy.rb
@@ -5,23 +5,23 @@ class AioProxy < Formula
 
   on_macos do
     on_arm do
-      url "https://registry.npmjs.org/@aio-proxy/cli-darwin-arm64/-/cli-darwin-arm64-0.1.0.tgz"
-      sha256 "c2a2468eb1a69180fb18da18714af9e9b7a929e4e53d5422a98bd3460aa6f201"
+      url "https://registry.npmjs.org/@aio-proxy/cli-darwin-arm64/-/cli-darwin-arm64-0.2.1.tgz"
+      sha256 "d50a6dc4639bb6e6e2c662be676e4b4a76118985c75b606d725dd977e33bd890"
     end
     on_intel do
-      url "https://registry.npmjs.org/@aio-proxy/cli-darwin-x64/-/cli-darwin-x64-0.1.0.tgz"
-      sha256 "bf4a771719869b3e4e2559a93f821bdb5c7e751fe6a497cef7a57f9e03455482"
+      url "https://registry.npmjs.org/@aio-proxy/cli-darwin-x64/-/cli-darwin-x64-0.2.1.tgz"
+      sha256 "03273d1de3a229fbe0175748477382ec4c06ecfdf178a02bb0814244627db9c5"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://registry.npmjs.org/@aio-proxy/cli-linux-arm64/-/cli-linux-arm64-0.1.0.tgz"
-      sha256 "e395b3615efa34b189bb3a27f779481e1d867825252d694afb6d99922d1e6697"
+      url "https://registry.npmjs.org/@aio-proxy/cli-linux-arm64/-/cli-linux-arm64-0.2.1.tgz"
+      sha256 "1fd5b644f6a1dbc35a7fb1b7bfddd62641494c65439702d1c627b3da83842cb8"
     end
     on_intel do
-      url "https://registry.npmjs.org/@aio-proxy/cli-linux-x64/-/cli-linux-x64-0.1.0.tgz"
-      sha256 "19262f01a43d504c9bdbe7c9700492503bee04def8a6387033c8ac2ebc66dea1"
+      url "https://registry.npmjs.org/@aio-proxy/cli-linux-x64/-/cli-linux-x64-0.2.1.tgz"
+      sha256 "49c90aa221e859d83177af8acc4407b1c217410fa1aa34faab02d90b8143b5e5"
     end
   end
 
